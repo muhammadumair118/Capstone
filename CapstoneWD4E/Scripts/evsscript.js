d6f3878683684cb43d26c8ede4f6d71eb917a182
Muhammad Umair <muhammadumair11111@gmail.com>
2020-09-05 13:45:40 +0500

    Add project files.

---
+++ CapstoneWD4E/Scripts/evsscript.js
@@ -0,0 +1,11 @@
+﻿$(".panel-heading").click(function () {
+    var anim = $(this).parent().data("hideanim");
+    if (anim == "slide") {
+        $(this).next().slideToggle(500);
+    }
+    else {
+        if (anim == "fade") {
+            $(this).next().fadeToggle(500);
+        }
+    }
+});
